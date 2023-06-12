ce08379cd8468c853e79da146c8880bcf38a5369
hieuNP <ryan.nguyen@genestory.ai>
2023-06-12 11:50:06 +0700

    adding log to module

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -8,6 +10,8 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/shop/greeting", func(c *fiber.Ctx) error {
+		response := "Hello World"
+		log.Println("Response:", response)
 		return c.SendString("Hello World")
 	})
 
